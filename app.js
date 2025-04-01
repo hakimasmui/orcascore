5ac01b72e9cba15265e7b35bf408e57eda2be017
Hakim <hakimasmui@gmail.com>
2025-04-01 17:46:18 +0700

    predict 2 apr and delete 25 mar

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function checkGoalPrediction(predict, homeScore, awayScore) {
 }
 
 (async () => {
-    const url = "https://www.goal.com/id/jadwal/2025-03-29";
+    const url = "https://www.goal.com/id/jadwal/2025-04-02";
     const tanggal_match = url.match(/(\d{4}-\d{2}-\d{2})/);
     const filename = tanggal_match[0].replaceAll("-", "")+".json";
     const url_call = "https://hakimasmui.github.io/orcascore/"+filename
@@ -105,11 +105,11 @@ async function crawlGaol(url, filename, tanggal_match, jsonArray) {
         } 
     }); 
     await page.waitForSelector("div.fco-competition-section");
-    const teams = [bayern, atm, "Real Sociedad", "Bayer Leverkusen", "Lyon", "Fenerbahce"];
-    const league = [spanyol, france, germany, "Turki - Super Lig"];
+    // const teams = [bayern, atm, "Real Sociedad", "Bayer Leverkusen", "Lyon", "Fenerbahce"];
+    // const league = [spanyol, france, germany, "Turki - Super Lig"];
 
-    // const teams = ["Juventus", "Roma", "Fiorentina", "Inter", realmadrid, barcelona, "Athletic Bilbao", mancity, "Aston Villa"];
-    // const league = [spanyol, italy, "Inggris Raya - FA Cup"];
+    const teams = [arsenal, "Wolverhampton Wanderers", manutd, realmadrid, "Arminia Bielefeld", "Paris Saint-Germain"];
+    const league = [inggris, "Italia - Coppa Italia", "Jerman - DFB-Pokal", "Prancis - Coupe de France"];
     let items = [];
     let tanggal;
     if (tanggal_match)

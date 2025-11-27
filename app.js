f3c30b03e39a03044feb5b155a29045b4a412d5c
hakim <hakimasmui@gmail.com>
2025-11-27 12:15:31 +0700

    eurpa league

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function checkGoalPrediction(predict, homeScore, awayScore) {
 }
 
 (async () => {
-    const url = "https://www.goal.com/id/jadwal/2025-11-27";
+    const url = "https://www.goal.com/id/jadwal/2025-11-28";
     const tanggal_match = url.match(/(\d{4}-\d{2}-\d{2})/);
     // const filename = tanggal_match[0].replaceAll("-", "")+".json";
     const filename = "matches.json";
@@ -110,8 +110,8 @@ async function crawlGaol(url, filename, tanggal_match, jsonArray) {
     }); 
     await page.waitForSelector("div.fco-competition-section");
 
-    const teams = ["Kairat Almaty", "Monaco", "Arsenal", "Inter", "Atalanta", "Liverpool", "Real Madrid", "Tottenham Hotspur", "Club Brugge"];
-    const league = ["International - Champions League"];
+    const teams = ["Freiburg", "Bologna", "Lyon", "Braga"];
+    const league = [europaleague];
     let items = [];
     let tanggal;
     if (tanggal_match)
